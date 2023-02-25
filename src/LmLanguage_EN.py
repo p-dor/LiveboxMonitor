bdc5b6a0e0b67670ab3eebe84c751f51ad462808
p-dor <dorpierre@orange.fr>
2023-02-25 21:21:20 +0100

    Partial English tooltips

---
+++ src/LmLanguage_EN.py
@@ -0,0 +1,92 @@
+### Livebox Monitor English Language module ###
+
+
+# ################################ LABELS ################################
+
+# No need - native application language
+
+
+# ################################ TOOLTIPS ################################
+
+TOOLTIPS = {
+	# Main window
+	'main': {
+		'deviceListTab': 'Manage the list of devices.',
+		'liveboxInfoTab': 'Detailed information about the Livebox and traffic statistics.',
+		'deviceInfoTab': 'Detailed information and setup for each device.',
+		'eventsTab': 'Raised events for each device.',
+		'dhcpTab': 'Detailed DHCP information and setup.',
+		'phoneTab': 'Manage phone calls and contact list.',
+		'actionTab': 'Various set of actions, preferences and application informations.',
+		'repeaterTab': 'Detailed information about the Wifi repeater and traffic statistics.'
+	},
+
+	# Device list
+	'dlist': {},
+
+	# Device list - IPv6 dialog
+	'ipv6': {},
+
+	# Livebox infos
+	'info': {},
+
+	# Device infos
+	'dinfo': {},
+
+	# Device infos - Assign name dialog
+	'dname': {},
+
+	# Device infos - Assign type dialog
+	'dtype': {},
+
+	# Events
+	'events': {},
+
+	# DHCP
+	'dhcp': {},
+
+	# DHCP - Binding dialog
+	'dbinding': {},
+
+	# DHCP - Setup dialog
+	'dsetup': {},
+
+	# Phone
+	'phone': {},
+
+	# Phone - Contact edit
+	'pcontact': {},
+
+	# Actions
+	'actions': {},
+
+	# Actions - Reboot history
+	'rhistory': {},
+
+	# Actions - Wifi global status
+	'wglobal': {},
+
+	# Repeater
+	'repeater': {},
+
+	# Config - Preferences
+	'prefs': {},
+
+	# Config - Connection
+	'cnx': {
+		'urlEdit': 'You must use for the connection exactly the same information as for the Livebox Web Interface\n'
+				   'For the URL try http://livebox.home/, http://livebox/ or http://192.168.1.1/.'
+	},
+
+	# Config - Signin
+	'signin': {
+		'userEdit': 'You must use for the connection exactly the same information as for the Livebox Web Interface\n'
+					'User must remain set to the default "admin".',
+		'passwordEdit': 'The password is either what you set up yourself or if you didn\'t change it, it is what is in\n'
+						'your Wifi card (corresponds to the first 8 characters, without spaces, of the security key visible\n'
+						'on the sticker at the back of your Livebox).'
+	}
+
+}
+
+  
