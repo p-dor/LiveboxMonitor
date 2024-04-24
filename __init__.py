10975efc26fbcbd8d576f3d29c47a54aa33b5130
Pierre Dor <dorpierre@orange.fr>
2024-04-24 17:20:08 +0200

    Release 1.4

--- __init__.py
+++ __init__.py
@@ -3,9 +3,9 @@
 __title__ = 'LiveboxMonitor'
 __description__ = 'Graphical user interface to monitor Livebox 4, 5, 6 & 7'
 __url__ = 'https://github.com/p-dor/LiveboxMonitor'
-__version__ = '1.4b'
+__version__ = '1.4'
 __build__ = 0x010400
 __author__ = 'Pierre Dor'
 __author_email__ = 'dorpierre@orange.fr'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2022-23 Pierre Dor'
+__copyright__ = 'Copyright 2022-24 Pierre Dor'
