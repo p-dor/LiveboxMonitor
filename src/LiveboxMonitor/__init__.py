ea0862e3ec90c5e71877e9360910283c38590d9e
Pierre Dor <dorpierre@orange.fr>
2025-04-11 15:08:28 +0200

    Version 1.5.2

--- src/LiveboxMonitor/__init__.py
+++ src/LiveboxMonitor/__init__.py
@@ -1,10 +1,10 @@
 # #### Livebox Monitor ####
 
 __title__ = 'LiveboxMonitor'
-__description__ = 'Graphical user interface to monitor Livebox 4, 5, 6 & 7'
+__description__ = 'Graphical user interface to manage Livebox 4, 5, 6 & 7'
 __url__ = 'https://github.com/p-dor/LiveboxMonitor'
-__version__ = '1.5.1'
-__build__ = 0x010501
+__version__ = '1.5.2'
+__build__ = 0x010502
 __author__ = 'Pierre Dor'
 __author_email__ = 'dorpierre@orange.fr'
 __license__ = 'MIT'
