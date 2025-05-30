9aefdf3b803afde89214340b74bfcce9e30b9047
Pierre Dor <dorpierre@orange.fr>
2025-05-30 17:25:18 +0200

    DHCP APIs

--- src/LiveboxMonitor/api/LmApiRegistry.py
+++ src/LiveboxMonitor/api/LmApiRegistry.py
@@ -4,6 +4,7 @@ from LiveboxMonitor.api.LmLiveboxInfoApi import LiveboxInfoApi
 from LiveboxMonitor.api.LmIntfApi import IntfApi
 from LiveboxMonitor.api.LmWifiApi import WifiApi
 from LiveboxMonitor.api.LmDeviceApi import DeviceApi
+from LiveboxMonitor.api.LmDhcpApi import DhcpApi
 from LiveboxMonitor.api.LmRebootApi import RebootApi
 from LiveboxMonitor.api.LmFirewallApi import FirewallApi
 from LiveboxMonitor.api.LmDynDnsApi import DynDnsApi
@@ -19,6 +20,7 @@ class ApiRegistry:
         self._intf = IntfApi(self)
         self._wifi = WifiApi(self)
         self._device = DeviceApi(self)
+        self._dhcp = DhcpApi(self)
         self._reboot = RebootApi(self)
         self._firewall = FirewallApi(self)
         self._dyndns = DynDnsApi(self)
