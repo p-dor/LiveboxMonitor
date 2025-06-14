4f8a169d8fa0c2a15da44845e5953d67cc4f724f
Pierre Dor <dorpierre@orange.fr>
2025-06-14 23:00:46 +0200

    NAT/PAT/Pinhole APIs

--- src/LiveboxMonitor/api/LmFirewallApi.py
+++ src/LiveboxMonitor/api/LmFirewallApi.py
@@ -42,7 +42,10 @@ class FirewallApi(LmApi):
 
     ### Get list of devices in DMZ, returns a dictionary
     def get_dmz_devices(self):
-        return self.call_no_check('Firewall', 'getDMZ')
+        d = self.call_no_check('Firewall', 'getDMZ')
+        if isinstance(d, dict):
+            return d
+        raise LmApiException('Firewall:getDMZ query error')
 
 
     ### Add a DMZ entry
@@ -59,3 +62,113 @@ class FirewallApi(LmApi):
     ### Delete a DMZ entry
     def delete_dmz(self, dmz_id):
         self.call('Firewall', 'deleteDMZ', {'id': dmz_id})
+
+
+    ### Get list of IPv4 port forwarding rules, returns a dictionary
+    # if no rule_id it returns all
+    # origin can be 'webui' or 'upnp', default is all
+    def get_ipv4_port_forwarding(self, rule_id=None, origin=None):
+        p = {}
+        if rule_id:
+            p['id'] = rule_id
+        if origin:
+            p['origin'] = origin
+        d = self.call_no_check('Firewall', 'getPortForwarding', p)
+        if isinstance(d, dict):
+            return d
+        raise LmApiException('Firewall:getPortForwarding query error')
+
+
+    ### Set a IPv4 port forwarding rules, returns the rule as the Livebox set it
+    # rule must follow Livebox model
+    def set_ipv4_port_forwarding(self, rule):
+        d = self.call_raw('Firewall', 'setPortForwarding', rule)
+        if d.get('status'):
+            d = d.get('data')
+            if d:
+                d = d.get('rule')
+            if d:
+                return d
+        raise LmApiException('Firewall:setPortForwarding query error')
+
+
+    ### Delete a IPv4 port forwarding rules
+    # origin can be 'webui' or 'upnp'
+    def del_ipv4_port_forwarding(self, rule_id, dest_ip, origin):
+        p = {'id': f'{origin}_{rule_id}',
+             'destinationIPAddress': dest_ip,
+             'origin': origin}
+        self.call('Firewall', 'deletePortForwarding', p)
+
+
+    ### Delete all IPv4 port forwarding rules
+    # origin can be 'webui' or 'upnp'
+    def del_all_ipv4_port_forwarding(self, origin):
+        self.call('Firewall', 'deletePortForwarding', {'origin': origin})
+
+
+    ### Get list of IPv4 protocol forwarding rules, returns a dictionary
+    def get_ipv4_protocol_forwarding(self):
+        d = self.call_no_check('Firewall', 'getProtocolForwarding')
+        if isinstance(d, dict):
+            return d
+        raise LmApiException('Firewall:getProtocolForwarding query error')
+
+
+    ### Set a IPv4 protocol forwarding rules, returns the rule as the Livebox set it
+    # rule must follow Livebox model
+    def set_ipv4_protocol_forwarding(self, rule):
+        d = self.call_raw('Firewall', 'setProtocolForwarding', rule)
+        if d.get('status'):
+            d = d.get('data')
+            if d:
+                d = d.get('rule')
+            if d:
+                return d
+        raise LmApiException('Firewall:setProtocolForwarding query error')
+
+
+    ### Delete a IPv4 protocol forwarding rules
+    def del_ipv4_protocol_forwarding(self, rule_id):
+        self.call('Firewall', 'deleteProtocolForwarding', {'id': rule_id})
+
+
+    ### Get list of IPv6 pinhole rules, returns a dictionary
+    # if no rule_id it returns all
+    # Origin can be 'webui' or 'upnp', default is all
+    def get_ipv6_pinhole(self, rule_id=None, origin=None):
+        p = {}
+        if rule_id:
+            p['id'] = rule_id
+        if origin:
+            p['origin'] = origin
+        d = self.call_no_check('Firewall', 'getPinhole', p)
+        if isinstance(d, dict):
+            return d
+        raise LmApiException('Firewall:getPinhole query error')
+
+
+    ### Set a IPv6 pinhole rules, returns the rule as the Livebox set it
+    # rule must follow Livebox model
+    def set_ipv6_pinhole(self, rule):
+        d = self.call_raw('Firewall', 'setPinhole', rule)
+        if d.get('status'):
+            d = d.get('data')
+            if d:
+                d = d.get('rule')
+            if d:
+                return d
+        raise LmApiException('Firewall:setPinhole query error')
+
+
+    ### Delete a IPv6 pinhole rules
+    # origin can be 'webui' or 'upnp'
+    def del_ipv6_pinhole(self, rule_id, origin):
+        p = {'id': f'{origin}_{rule_id}',
+             'origin': origin}
+        self.call('Firewall', 'deletePinhole', p)
+
+
+    ### Commit a change
+    def commit(self):
+        self.call('Firewall', 'commit')
