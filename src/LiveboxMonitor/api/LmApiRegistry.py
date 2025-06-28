3a2dcf26d25ff131b47caa4238804c65d612d30d
Pierre Dor <dorpierre@orange.fr>
2025-06-28 18:29:47 +0200

    Repeater support & session closing

--- src/LiveboxMonitor/api/LmApiRegistry.py
+++ src/LiveboxMonitor/api/LmApiRegistry.py
@@ -17,8 +17,9 @@ from LiveboxMonitor.api.LmScreenApi import ScreenApi
 
 # ################################ API objects registry ################################
 class ApiRegistry:
-    def __init__(self, session):
+    def __init__(self, session, is_repeater=False):
         self._session = session
+        self._is_repeater = is_repeater
         self._info = LiveboxInfoApi(self)
         self._intf = IntfApi(self)
         self._wifi = WifiApi(self)
@@ -32,3 +33,23 @@ class ApiRegistry:
         self._dyndns = DynDnsApi(self)
         self._backup = BackupRestoreApi(self)
         self._screen = ScreenApi(self)
+
+
+    ### Close the session
+    def close(self):
+        if self._session:
+            self._session.close()
+            self._session = None
+        self._info.session_closed()
+        self._intf.session_closed()
+        self._wifi.session_closed()
+        self._device.session_closed()
+        self._stats.session_closed()
+        self._dhcp.session_closed()
+        self._voip.session_closed()
+        self._iptv.session_closed()
+        self._reboot.session_closed()
+        self._firewall.session_closed()
+        self._dyndns.session_closed()
+        self._backup.session_closed()
+        self._screen.session_closed()
